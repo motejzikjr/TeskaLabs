fbae1c57738b24c76826ae1643c04056596a7072
Jiří Motejzík <motejzik@praguebest.cz>
2026-04-14 12:47:46 +0200

    add translations

--- src/module/users/components/Detail.jsx
+++ src/module/users/components/Detail.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { useParams } from "react-router";
 import { useDetail } from "../hooks/useDetail";
 import { Spinner, ResultCard, DateTime } from "asab_webui_components";
+import { useTranslation } from 'react-i18next';
 
 export const Detail = () => {
     let params = useParams();
+    const { t } = useTranslation();
 
     const { user, loading, error } = useDetail(params.id)
 
@@ -14,12 +16,12 @@ export const Detail = () => {
     return (
         <ResultCard status='success'>
             <h1>{user.username}</h1>
-            <p><strong>E-mail:</strong> {user.email}</p>
-            <p><strong>Address:</strong> {user.address}</p>
-            <p><strong>Created at:</strong> <DateTime value={user.created}/></p>
-            <p><strong>Id:</strong> {user.id}</p>
-            <p><strong>IP address:</strong> {user.ip_address}</p>
-            <p><strong>Phone number:</strong> {user.phone_number}</p>
+            <p><strong>{t("Users|E-mail")}:</strong> {user.email}</p>
+            <p><strong>{t("Users|Address")}:</strong> {user.address}</p>
+            <p><strong>{t("Users|Created at")}:</strong> <DateTime value={user.created}/></p>
+            <p><strong>{t("Users|Id")}:</strong> {user.id}</p>
+            <p><strong>{t("Users|IP address")}:</strong> {user.ip_address}</p>
+            <p><strong>{t("Users|Phone number")}:</strong> {user.phone_number}</p>
         </ResultCard>
     );
 }
